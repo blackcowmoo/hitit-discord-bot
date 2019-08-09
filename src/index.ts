20a17086e869aa865d3911d32fea6298d53ec0f5
Micalgenus <micalgenus@gmail.com>
2019-08-09 23:18:37 +0900

    Update method and change to get

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,24 @@ import lolchess from '@/lolchess';
 
 import { app } from '@/lib/express';
 
+const commandParser = async (command: string, ...options: string[]): Promise<string> => {
+  switch (command) {
+    case '!lolchess':
+    case '!롤토체스':
+    case '!롤체':
+      return lolchess(options);
+    case '!내바박':
+      return lolchess(['고스트', '체스왕']);
+    case '!크로스핏':
+      return 'https://namu.wiki/w/크로스핏';
+  }
+};
+
 app.use('/', async (req, res) => {
   try {
-    if (!req.body.content) return res.status(404).send('');
-
-    const [command, ...options] = req.body.content.split(' ');
-    switch (command) {
-      case '!lolchess':
-      case '!롤토체스':
-      case '!롤체':
-        return res.send(lolchess(options));
-      case '!내바박':
-        return res.send(lolchess(['고스트', '체스왕']));
-      case '!크로스핏':
-        return res.send('https://namu.wiki/w/크로스핏');
-    }
+    if (!req.query.content) return res.status(404).send('');
+    const [command, ...options] = req.query.content.split(' ');
+    return res.send(await commandParser(command, options));
   } catch (err) {
     return res.status(500).send('');
   }
