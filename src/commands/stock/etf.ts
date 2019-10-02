dd6ea6bc927e6689e3b4fb7fa79cb2376efb839a
Micalgenus <micalgenus@gmail.com>
2019-10-02 21:53:00 +0900

    Refactor stock for investing.com

--- src/commands/stock/etf.ts
+++ src/commands/stock/etf.ts
@@ -6,7 +6,7 @@ import rp from 'request-promise';
 import cheerio from 'cheerio';
 import { makePrice } from '@/lib/utils/number';
 
-export const voo = async (): Promise<string> => {
+const getStockFromInvesting = async (id: number) => {
   const URL = 'https://kr.investing.com/etfs/vanguard-s-p-500';
   const html: string = await rp({
     method: 'GET',
@@ -15,7 +15,7 @@ export const voo = async (): Promise<string> => {
   });
   const $ = cheerio.load(html);
 
-  const price = $('.inlineblock.pid-38165-last').text();
+  const price = $(`.inlineblock.pid-${id}-last`).text();
   const dollar = $('.js-item-last.pid-650-last').text();
 
   const voo = {
@@ -29,48 +29,6 @@ export const voo = async (): Promise<string> => {
     .join('\n');
 };
 
-export const agg = async (): Promise<string> => {
-  const URL = 'https://kr.investing.com/etfs/ishares-barclays-agg';
-  const html: string = await rp({
-    method: 'GET',
-    uri: URL,
-    headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36' },
-  });
-  const $ = cheerio.load(html);
-
-  const price = $('.inlineblock.pid-503-last').text();
-  const dollar = $('.js-item-last.pid-650-last').text();
-
-  const voo = {
-    가격: price,
-    환율: dollar,
-    한국가격: makePrice(+price.replace(',', '') * +dollar.replace(',', '')),
-  };
-
-  return Object.entries(voo)
-    .map(e => `${e[0]}: ${e[1]}`)
-    .join('\n');
-};
-
-export const tvix = async (): Promise<string> => {
-  const URL = 'https://kr.investing.com/etfs/velocityshares-dly-2x-vix-sh.-term-historical-data';
-  const html: string = await rp({
-    method: 'GET',
-    uri: URL,
-    headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36' },
-  });
-  const $ = cheerio.load(html);
-
-  const price = $('.inlineblock.pid-38153-last').text();
-  const dollar = $('.js-item-last.pid-650-last').text();
-
-  const voo = {
-    가격: price,
-    환율: dollar,
-    한국가격: makePrice(+price.replace(',', '') * +dollar.replace(',', '')),
-  };
-
-  return Object.entries(voo)
-    .map(e => `${e[0]}: ${e[1]}`)
-    .join('\n');
-};
+export const voo = async (): Promise<string> => await getStockFromInvesting(38165);
+export const agg = async (): Promise<string> => await getStockFromInvesting(503);
+export const tvix = async (): Promise<string> => await getStockFromInvesting(38153);
